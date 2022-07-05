cfda9615bb1cca8ae7d710ff74d0bbc66ef2a583
Prashanth Puranik <puranik@digdeeper.in>
2022-07-05 15:15:36 +0530

    using font awesome icons

--- workshops-app/src/components/pages/WokshopDetails/index.jsx
+++ workshops-app/src/components/pages/WokshopDetails/index.jsx
@@ -1,10 +1,19 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Alert, Row, Col, Image } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faCheck,
+    faTimes
+} from '@fortawesome/free-solid-svg-icons';
+
+import Moment from 'react-moment';
 
 import { getWorkshopById } from '../../../services/workshops';
 
 const WorkshopDetails = () => {
+    const format = "DD-MM-yyyy";
+
     const { id } = useParams(); // { id: '2', action: 'edit' }
 
     const [ workshop, setWorkshop ] = useState( null );
@@ -43,7 +52,37 @@ const WorkshopDetails = () => {
                         <Col xs={12} lg={4}>
                             <Image src={workshop.imageUrl} fluid />
                         </Col>
-                        <Col xs={12} lg={8} dangerouslySetInnerHTML={{ __html: workshop.description }}></Col>
+                        <Col xs={12} lg={8}>
+                            <Row xs={2} lg={4}>
+                                <Col>
+                                    <div>
+                                        <small>
+                                            <Moment format={format}>
+                                                {workshop.startDate}
+                                            </Moment>
+                                            {" - "}
+                                            <Moment format={format}>{workshop.endDate}</Moment>
+                                        </small>
+                                    </div>
+                                    <div>
+                                        <small>{workshop.time}</small>
+                                    </div>
+                                </Col>
+                                <Col>
+                                    <div>
+                                        {/* EXERCISE: display check / times icon based on workshop.modes.inPerson */}
+                                        <FontAwesomeIcon icon={faCheck} />
+                                        <span className="ms-2">In-Person</span>
+                                        </div>
+                                    <div>
+                                        {/* EXERCISE: display check / times icon based on workshop.modes.online */}
+                                        <FontAwesomeIcon icon={faTimes} />
+                                        <span className="ms-2">Online</span>
+                                    </div>
+                                </Col>
+                            </Row>
+                            <div dangerouslySetInnerHTML={{ __html: workshop.description }}></div>
+                        </Col>
                     </Row>
                 </div>
             )
