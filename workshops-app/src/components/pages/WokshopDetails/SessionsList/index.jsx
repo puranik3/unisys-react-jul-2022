d524c4ae8d69021cdaf3a4c7af62c76bba316e10
Prashanth Puranik <puranik@digdeeper.in>
2022-07-06 15:50:54 +0530

    setting up a controlled component (value, onChange)

--- workshops-app/src/components/pages/WokshopDetails/SessionsList/index.jsx
+++ workshops-app/src/components/pages/WokshopDetails/SessionsList/index.jsx
@@ -1,21 +1,25 @@
-import { useState, useEffect } from 'react';
-import { Spinner, Alert, ListGroup } from 'react-bootstrap';
-import SessionItem from './SessionItem';
-import { toast } from 'react-toastify';
+import { useState, useEffect } from "react";
+import { Form, Spinner, Alert, ListGroup } from "react-bootstrap";
+import SessionItem from "./SessionItem";
+import { toast } from "react-toastify";
 
-import { getSessionsForWorkshop, vote as voteSvc } from "../../../../services/sessions";
+import {
+    getSessionsForWorkshop,
+    vote as voteSvc,
+} from "../../../../services/sessions";
 
 // The component recieves id as a prop. Alternatively it can use useParams() to get it from the URL.
-const SessionsList = ( { id } ) => {
+const SessionsList = ({ id }) => {
     const [sessions, setSessions] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [filterKey, setFilterKey] = useState("Props");
 
     useEffect(() => {
         const fetchSessionsForWorkshop = async () => {
             try {
-                const sessions = await getSessionsForWorkshop( id );
-                setSessions( sessions );
+                const sessions = await getSessionsForWorkshop(id);
+                setSessions(sessions);
             } catch (error) {
                 setError(error);
             } finally {
@@ -27,34 +31,32 @@ const SessionsList = ( { id } ) => {
         fetchSessionsForWorkshop();
     }, []); // runs ONLY after first render
 
-    const vote = async ( event, sessionId, type ) => {
-        console.log( event );
-        console.log( 'You tried to ' + type + ' session id =' + sessionId );
+    const vote = async (event, sessionId, type) => {
+        console.log(event);
+        console.log("You tried to " + type + " session id =" + sessionId);
 
         try {
-            const updatedSession = await voteSvc( sessionId, type );
-            console.log( updatedSession );
+            const updatedSession = await voteSvc(sessionId, type);
+            console.log(updatedSession);
 
-            sessions.find( session => session.id === updatedSession.id );
+            sessions.find((session) => session.id === updatedSession.id);
 
             // we make an "immutable" change to the sessions array - i.e. the current sessions remains untouched, and a new array of sessions is generated
-            setSessions( sessions => {
+            setSessions((sessions) => {
                 // generate a new sessions array through map(), and return it
                 // every item except the one updated is the same as before
-                return sessions.map(
-                    s => {
-                        if( s.id === sessionId ) {
-                            return updatedSession;
-                        } else {
-                            return s;
-                        }
+                return sessions.map((s) => {
+                    if (s.id === sessionId) {
+                        return updatedSession;
+                    } else {
+                        return s;
                     }
-                )
+                });
             });
 
-            toast.success( 'Your vote has been captured' );
-        } catch( error ) {
-            toast.error( error.message );
+            toast.success("Your vote has been captured");
+        } catch (error) {
+            toast.error(error.message);
         }
     };
 
@@ -70,30 +72,34 @@ const SessionsList = ( { id } ) => {
             {!loading && error && (
                 <Alert variant="danger">{error.message}</Alert>
             )}
-            {
-                !loading && !error && (
-                    <>
-                        <h2>Sessions in this workshop</h2>
-                        <hr />
-                        <ListGroup>
-                            {
-                                sessions.map(
-                                    session => (
-                                        <ListGroup.Item key={session.id}>
-                                            <SessionItem
-                                                session={session}
-                                                vote={vote}
-                                            ></SessionItem>
-                                        </ListGroup.Item>
-                                    )
-                                )
-                            }
-                        </ListGroup>
-                    </>
-                )
-            }
+            {!loading && !error && (
+                <>
+                    <h2>Sessions in this workshop</h2>
+                    <hr />
+                    {/* value prop when set, enables React to "control" the component's value */}
+                    <Form.Group className="mb-3">
+                        <Form.Control
+                            type="search"
+                            placeholder="Type here to search by session name"
+                            value={filterKey}
+                            onChange={( event ) => setFilterKey( event.target.value )}
+                        />
+                    </Form.Group>
+                    {filterKey}
+                    <ListGroup>
+                        {sessions.map((session) => (
+                            <ListGroup.Item key={session.id}>
+                                <SessionItem
+                                    session={session}
+                                    vote={vote}
+                                ></SessionItem>
+                            </ListGroup.Item>
+                        ))}
+                    </ListGroup>
+                </>
+            )}
         </div>
     );
-}
- 
+};
+
 export default SessionsList;
