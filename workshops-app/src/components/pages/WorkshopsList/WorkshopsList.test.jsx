04f080b8f2b0a6933c1da9fbbfbf735a41409c74
Prashanth Puranik <puranik@digdeeper.in>
2022-07-06 14:27:54 +0530

    workshop names appear after initial load

--- workshops-app/src/components/pages/WorkshopsList/WorkshopsList.test.jsx
+++ workshops-app/src/components/pages/WorkshopsList/WorkshopsList.test.jsx
@@ -1,6 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import { getByText, render, screen } from "@testing-library/react";
 import WorkshopsList from "./";
 import AllProviders from '../../../test-utils/AllProviders';
+
+import workshops from '../../../mocks/data/workshops.json';
 
 // "test suite" (group of tests)
 describe( 'loading tests', () => {
@@ -15,5 +17,21 @@ describe( 'loading tests', () => {
         // const loadingEl = screen.g`etByText( /Loading/i );
         
         expect( loadingEl ).toBeInTheDocument();
+    });
+
+    test( 'shows the workshops after they are fetched on load', async () => {
+        render(
+            <AllProviders>
+                <WorkshopsList />
+            </AllProviders>
+        );
+
+        const workshopsPage1 = workshops.slice( 0, 10 );
+
+        for( let i = 0; i < workshopsPage1.length; i++ ) {
+            // we use findBy*() methods when we expect the element to be matched in the near future
+            const workshopCardTitle = await screen.findByText( workshops[i].name )
+            expect( workshopCardTitle ).toHaveTextContent( workshops[i].name );
+        }
     });
 });
