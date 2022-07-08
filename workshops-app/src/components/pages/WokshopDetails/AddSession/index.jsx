6566981fcb26dff78dfb4398f37bfbacab606cc2
Prashanth Puranik <puranik@digdeeper.in>
2022-07-08 09:32:35 +0530

    react hook form

--- workshops-app/src/components/pages/WokshopDetails/AddSession/index.jsx
+++ workshops-app/src/components/pages/WokshopDetails/AddSession/index.jsx
@@ -5,108 +5,72 @@ import { Form, Button } from "react-bootstrap";
 import { useHistory } from 'react-router-dom';
 
 import { addSession as addSessionSvc } from '../../../../services/sessions';
+import { useForm } from 'react-hook-form';
 
 const AddSession = ({ id }) => {
+    /**
+     *  errors = {
+     *      sequenceId: {
+     *          type: "required"
+     *      }
+     *  }
+     */
+    const { register, getValues, handleSubmit, formState: { errors } } = useForm({
+        mode: 'all'
+    });
+
     const { push } = useHistory();
 
-    const [ sequenceId, setSequenceId ] = useState( '' );
-    const [ name, setName ] = useState( '' );
-    const [ speaker, setSpeaker ] = useState( '' );
-    const [ duration, setDuration ] = useState( '' );
-    const [ level, setLevel ] = useState( '' );
-    const [ abstract, setAbstract ] = useState( '' );
+    const addSession = async ( incomingSession ) => {
+        const session = {
+            workshopId: id,
+            upvoteCount: 0,
+            ...incomingSession,
+            sequenceId: +incomingSession.sequenceId,
+            duration: +incomingSession.duration
+        };
 
-    const [ sequenceIdErr, setSequenceIdErr ] = useState( '' );
-    // const [ nameErr, setNameErr ] = useState( '' );
-    // const [ speakerErr, setSpeakerErr ] = useState( '' );
-    // const [ durationErr, setDurationErr ] = useState( '' );
-    // const [ levelErr, setLevelErr ] = useState( '' );
-    // const [ abstractErr, setAbstractErr ] = useState( '' );
-
-    const [ valid, setValid ] = useState( false );
-    const [ isSubmitted, setSubmitted ] = useState( false );
-
-    const validate = () => {
-        const sequenceIdVal = sequenceId.trim();
-        
-        let sequenceIdErr = '';
-        let valid = true;
-
-        // is required
-        if( sequenceIdVal === '' ) {
-            sequenceIdErr += 'Sequence ID cannot be empty';
-            valid = false;
+        try {
+            const addedSession = await addSessionSvc( session );
+            toast.success( 'Session was added' );
+            push( `/workshops/${id}` );
+        } catch( error ) {
+            console.log( error );
+            toast.error( error.response.data || error.message )
         }
+    }
 
-        // is a positive number
-        if( /^\d+$/.test( sequenceIdVal ) === false ) {
-            sequenceIdErr += 'Sequence ID must be a positive number';
-            valid = false;
-        }
+    const csv = () => {
+        const csvPattern = /^[A-Za-z][A-Za-z ]*(,[A-Za-z ]+)*$/;
+        const speakerValue = getValues( 'speaker' );
 
-        setSequenceIdErr( sequenceIdErr );
-
-        // validate other inputs similarly
-        // ...
-
-        setValid( valid );
-
-        return valid;
+        return csvPattern.test( speakerValue );
     };
 
-    useEffect( () => {
-        validate();
-    }, [ sequenceId ] );
-
-    const addSession = async ( event ) => {
-        setSubmitted( true );
-
-        // prevent the browser from submitting the form
-        event.preventDefault();
-
-        if( validate() ) {
-            const session = {
-                workshopId: +id,
-                sequenceId: +sequenceId.trim(),
-                name: name.trim(),
-                speaker: speaker.trim(),
-                duration: parseFloat( duration.trim() ),
-                level: level.trim(),
-                abstract: abstract.trim(),
-                upvoteCount: 0
-            };
-
-            try {
-                const addedSession = await addSessionSvc( session );
-                toast.success( 'Session was added' );
-                push( `/workshops/${id}` );
-            } catch( error ) {
-                console.log( error );
-                toast.error( error.response.data || error.message )
-            }
-        }
+    const p = {
+        type: "number",
+        placeholder: "2"
     }
 
     return (
         <>
             <h2 className="mt-4">Add a session</h2>
             <hr />
-            <Form onSubmit={addSession}>
+            <Form onSubmit={handleSubmit(addSession)}>
                 <Form.Group className="mb-3" controlId="sequenceId">
                     <Form.Label>Sequence ID</Form.Label>
                     <Form.Control
-                        type="number"
-                        placeholder="2"
-                        value={sequenceId}
-                        onChange={ event => setSequenceId( event.target.value ) }
-                        isInvalid={isSubmitted && sequenceIdErr}
+                        {...p}
+                        {...register( 'sequenceId', { required: true, pattern: /^\d+$/ } )}
+                        isInvalid={errors.sequenceId?.type}
                     />
                     <Form.Text className="text-muted">
                         Sequence ID is the serial number of the session in the
                         workshop
                     </Form.Text>
                     <Form.Control.Feedback type="invalid">
-                        {isSubmitted && sequenceIdErr}
+                        {errors.sequenceId?.type === 'required' && <div>Sequence ID is required</div>}
+                        {errors.sequenceId?.type === 'pattern' && <div>Sequence ID must be a positive number</div>}
                     </Form.Control.Feedback>
                 </Form.Group>
 
@@ -115,9 +79,12 @@ const AddSession = ({ id }) => {
                     <Form.Control
                         type="text"
                         placeholder="React v18 features"
-                        value={name}
-                        onChange={ event => setName( event.target.value ) }
+                        {...register( 'name', { required: true } )}
+                        isInvalid={errors.name?.type}
                     />
+                    <Form.Control.Feedback type="invalid">
+                        {errors.name?.type === 'required' && <div>Name is required</div>}
+                    </Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="speaker">
@@ -125,9 +92,13 @@ const AddSession = ({ id }) => {
                     <Form.Control
                         type="text"
                         placeholder="John Doe, Jane Doe"
-                        value={speaker}
-                        onChange={ event => setSpeaker( event.target.value ) }
+                        {...register( 'speaker', { required: true, validate: { csv: csv } } )}
+                        isInvalid={errors.speaker?.type}
                     />
+                    <Form.Control.Feedback type="invalid">
+                        {errors.speaker?.type === 'required' && <div>At least one speaker's name required</div>}
+                        {errors.speaker?.type === 'csv' && <div>Speaker names can have only letters and spaces. Separate speaker names with a comma.</div>}
+                    </Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="duration">
@@ -135,26 +106,33 @@ const AddSession = ({ id }) => {
                     <Form.Control
                         type="text"
                         placeholder="1.5"
-                        value={duration}
-                        onChange={ event => setDuration( event.target.value ) }
+                        {...register( 'duration', { required: true, pattern: /^\d+(\.\d+)?$/ } )}
+                        isInvalid={errors.duration?.type}
                     />
                     <Form.Text className="text-muted">
                         Duration must be specified in hours. Example: for 30
                         minutes, enter 0.5
                     </Form.Text>
+                    <Form.Control.Feedback type="invalid">
+                        {errors.duration?.type === 'required' && <div>Duration is required</div>}
+                        {errors.duration?.type === 'pattern' && <div>Duration must be a positive number (with optional decimal part)</div>}
+                    </Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="level">
                     <Form.Label>Level</Form.Label>
                     <Form.Select
-                        value={level}
-                        onChange={ event => setLevel( event.target.value ) }
+                        {...register( 'level', { required: true } )}
+                        isInvalid={errors.level?.type}
                     >
                         <option value="">-- Select level --</option>
                         <option value="Basic">Basic</option>
                         <option value="Intermediate">Intermediate</option>
                         <option value="Advanced">Advanced</option>
                     </Form.Select>
+                    <Form.Control.Feedback type="invalid">
+                        {errors.level?.type === 'required' && <div>Level is required</div>}
+                    </Form.Control.Feedback>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="abstract">
@@ -162,12 +140,16 @@ const AddSession = ({ id }) => {
                     <Form.Control
                         as="textarea"
                         placeholder="Describe the session in a few words"
-                        value={abstract}
-                        onChange={ event => setAbstract( event.target.value ) }
+                        {...register( 'abstract', { required: true, minLength: 20 } )}
+                        isInvalid={errors.abstract?.type}
                     />
                     <Form.Text className="text-muted">
                         Description must be at least 20 characters long
                     </Form.Text>
+                    <Form.Control.Feedback type="invalid">
+                        {errors.abstract?.type === 'required' && <div>Description is required</div>}
+                        {errors.abstract?.minLength === 'required' && <div>Description must be at least 20 characters long</div>}
+                    </Form.Control.Feedback>
                 </Form.Group>
 
                 <Button variant="primary" type="submit">
