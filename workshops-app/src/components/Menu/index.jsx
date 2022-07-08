c03371d37395f7016881bcda25b596c8f7937fda
Prashanth Puranik <puranik@digdeeper.in>
2022-07-08 11:49:32 +0530

    changing the theme using context API

--- workshops-app/src/components/Menu/index.jsx
+++ workshops-app/src/components/Menu/index.jsx
@@ -1,21 +1,55 @@
-import { Navbar, Container, Nav } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import { Navbar, Container, Nav, ToggleButton, ToggleButtonGroup } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { useContext } from 'react';
+import ThemeContext from "../../context/ThemeContext";
 
 const Menu = () => {
-    const activeClassName = 'active';
+    const { theme, toggleTheme } = useContext( ThemeContext );
+
+    const activeClassName = "active";
 
     return (
-        <Navbar bg="light" variant="light">
+        <Navbar variant={theme} bg={theme}>
             <Container>
                 {/* NavLink will apply a CSS class "active" on the link whose "to" matches the current URL path */}
 
                 {/* Use "active" prop to avoid applying "active" CSS class when URL path is /workshops (for eg.) */}
 
                 {/* If the active class name is different from "active" then use activeClassName prop to set it */}
-                <Navbar.Brand to="/" as={NavLink} exact activeClassName={activeClassName}>Home</Navbar.Brand>
+                <Navbar.Brand
+                    to="/"
+                    as={NavLink}
+                    exact
+                    activeClassName={activeClassName}
+                >
+                    Home
+                </Navbar.Brand>
                 <Nav className="me-auto">
-                    <Nav.Link to={{ pathname: "/workshops", search: '?start=2' }} as={NavLink}>List of workshops</Nav.Link>
-                    <Nav.Link to="/feedback" as={NavLink}>Feedback</Nav.Link>
+                    <Nav.Link
+                        to={{ pathname: "/workshops", search: "?start=2" }}
+                        as={NavLink}
+                    >
+                        List of workshops
+                    </Nav.Link>
+                    <Nav.Link to="/feedback" as={NavLink}>
+                        Feedback
+                    </Nav.Link>
+                </Nav>
+                <Nav>
+                    <ToggleButtonGroup
+                        type="radio"
+                        defaultValue={"light"}
+                        className="mb-2"
+                        name="tbg-theme"
+                        onChange={value => toggleTheme( value )}
+                    >
+                        <ToggleButton id="tbg-light" value="light">
+                            Light
+                        </ToggleButton>
+                        <ToggleButton id="tbg-dark" value="dark">
+                            Dark
+                        </ToggleButton>
+                    </ToggleButtonGroup>
                 </Nav>
             </Container>
         </Navbar>
